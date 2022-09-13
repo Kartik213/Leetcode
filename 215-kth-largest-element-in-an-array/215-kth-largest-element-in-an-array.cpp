548ac48d2a7a1a32646fdfea298e9205e6688563
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-09-13 19:00:04 +0530

    Time: 177 ms (50.15%), Space: 45.6 MB (67.50%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    // void heapify(vector<int>& nums,int i){
-    //     int largest = i;
-    //     int left = 2*i +1;
-    //     int right = 2*i + 2;
-    //     if(left<nums.size() && nums[left]>nums[largest]){
-    //         largest = left;
-    //     }
-    //     if(right<nums.size() && nums[right]>nums[largest]){
-    //         largest = right;
-    //     }
-    //     if(largest != i){
-    //         swap(nums[i],nums[largest]);
-    //         heapify(nums,largest);
-    //     }
-    // }
-    // void deletion(vector<int>& nums){
-    //     if(nums.size() == 0)
-    //         return ;
-    //     nums[0] = nums[nums.size()-1];
-    //     nums.pop_back();
-    //     heapify(nums,0);
-    // }
+    void heapify(vector<int>& nums,int i){
+        int largest = i;
+        int left = 2*i +1;
+        int right = 2*i + 2;
+        if(left<nums.size() && nums[left]>nums[largest]){
+            largest = left;
+        }
+        if(right<nums.size() && nums[right]>nums[largest]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(nums[i],nums[largest]);
+            heapify(nums,largest);
+        }
+    }
+    void deletion(vector<int>& nums){
+        if(nums.size() == 0)
+            return ;
+        nums[0] = nums[nums.size()-1];
+        nums.pop_back();
+        heapify(nums,0);
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        // int n = nums.size();
-        // for(int i=(n/2)-1;i>=0;i--){
-        //     heapify(nums,i);
-        // }
-        // for(int i=k-1;i>0;i--){
-        //     deletion(nums);
-        // }
-        // return nums[0];
-        priority_queue<int>pq;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            pq.push(nums[i]);
+        for(int i=(n/2)-1;i>=0;i--){
+            heapify(nums,i);
         }
-        int i = k-1;
-        while(i>0){
-            pq.pop();
-            i--;
+        for(int i=0;i<k-1;i++){
+            deletion(nums);
         }
-        return pq.top();
+        return nums[0];
+        // priority_queue<int>pq;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     pq.push(nums[i]);
+        // }
+        // int i = k-1;
+        // while(i>0){
+        //     pq.pop();
+        //     i--;
+        // }
+        // return pq.top();
     }
 };
