351997f3306b78f954166e409c5828f431ce3ba3
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-09-26 19:32:49 +0530

    Time: 35 ms (47.51%), Space: 10.2 MB (33.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -7,14 +7,21 @@ public:
         if(s.length() == 0)
             return 0;
         while(j<s.length()){
+            //mapping the character
             mp[s[j]]++;
+            //if map size is equal to the window the window size this means substring has all unique characters
+            //and can check for result
             if(mp.size() == j-i+1){
                 maxi = max(maxi,(j-i+1));
                 j++;
             }
+            //if map size is less than window size this means it has some duplicate characters
+            //remove the characters while map size is less than the window size
+            //this will remove the duplicate elements from the map
             else if(mp.size()<j-i+1){
                 while(mp.size()<j-i+1){
                     mp[s[i]]--;
+                    //if the map value of a character became zero the remove it from the map
                     if(mp[s[i]] == 0)
                         mp.erase(s[i]);
                     i++;
