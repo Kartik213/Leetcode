1da63dc4f4072156fa7a04ef26576ff4b1f6d91f
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-09-28 18:41:02 +0530

    Time: 51 ms (73.21%), Space: 27.5 MB (63.09%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int low, int high){
-        if(high >= low){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid]>target)
-                return binarySearch(nums,target,low,mid-1);
-            else
-                return binarySearch(nums,target,mid+1,high);
-        }
-        return -1;
-    }
+    // int binarySearch(vector<int>& nums, int target, int low, int high){
+    //     if(high >= low){
+    //         int mid = low + (high - low)/2;
+    //         if(nums[mid] == target)
+    //             return mid;
+    //         else if(nums[mid]>target)//target lies on the right side
+    //             return binarySearch(nums,target,low,mid-1);
+    //         else//target lies on the left
+    //             return binarySearch(nums,target,mid+1,high);
+    //     }
+    //     return -1;
+    // }
     int search(vector<int>& nums, int target) {
         int low=0,high=nums.size()-1;
-        // while(low<=high){
-        //     int mid = (low+high)/2;
-        //     if(nums[mid] == target)
-        //         return mid;
-        //     else if(nums[mid]>target)
-        //         high = mid-1;
-        //     else
-        //         low = mid+1;
-        // }
-        // return -1;
-        return binarySearch(nums,target,low,high);
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid]>target)//target lies on the right side
+                high = mid-1;
+            else//target lies on the left
+                low = mid+1;
+        }
+        return -1;
+        // return binarySearch(nums,target,low,high);
     }
 };
