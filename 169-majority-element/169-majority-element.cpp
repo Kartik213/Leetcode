473b9c2675d684c6e6573ef07806eeea6cf98c88
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-10-06 15:06:24 +0530

    Time: 50 ms (17.78%), Space: 19.6 MB (59.75%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -6,8 +6,10 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++){
             mp[nums[i]]++;
-            if(mp[nums[i]] > n/2)
+            if(mp[nums[i]] > n/2){
                 x = nums[i];
+                break;
+            }
         }
         return x;
     }
