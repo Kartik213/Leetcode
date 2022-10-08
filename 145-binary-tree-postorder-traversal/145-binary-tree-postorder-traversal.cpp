9bfe7477bb30d1afbe86f513655aae9f2145bea0
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-10-08 18:21:34 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (36.69%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -18,31 +18,54 @@ public:
     //     postorder(root->right,res);
     //     res.push_back(root->val);
     // }
+    //Using 1 stack
+    // void postorder(TreeNode* root, vector<int>& res){
+    //     TreeNode* current = root;
+    //     stack<TreeNode*>st;
+    //     while(current != NULL || !st.empty()){
+    //         if(current != NULL){
+    //             st.push(current);
+    //             current = current -> left;
+    //         }
+    //         else{
+    //             TreeNode* temp = st.top();
+    //             temp = temp->right;
+    //             if(temp == NULL){
+    //                 temp = st.top();
+    //                 st.pop();
+    //                 res.push_back(temp->val);
+    //                 while(!st.empty() && temp == st.top()->right){
+    //                     temp = st.top();
+    //                     st.pop();
+    //                     res.push_back(temp->val);
+    //                 }
+    //             }
+    //             else{
+    //                 current = temp;
+    //             }
+    //         }
+    //     }
+    // }
     void postorder(TreeNode* root, vector<int>& res){
-        TreeNode* current = root;
-        stack<TreeNode*>st;
-        while(current != NULL || !st.empty()){
-            if(current != NULL){
-                st.push(current);
-                current = current -> left;
+        stack<TreeNode*>s1;
+        stack<TreeNode*>s2;
+        if(root == NULL)
+            return ;
+        s1.push(root);
+        while(!s1.empty()){
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left != NULL){
+                s1.push(root->left);
             }
-            else{
-                TreeNode* temp = st.top();
-                temp = temp->right;
-                if(temp == NULL){
-                    temp = st.top();
-                    st.pop();
-                    res.push_back(temp->val);
-                    while(!st.empty() && temp == st.top()->right){
-                        temp = st.top();
-                        st.pop();
-                        res.push_back(temp->val);
-                    }
-                }
-                else{
-                    current = temp;
-                }
+            if(root->right != NULL){
+                s1.push(root->right);
             }
+        }
+        while(!s2.empty()){
+            res.push_back(s2.top()->val);
+            s2.pop();
         }
     }
     vector<int> postorderTraversal(TreeNode* root) {
