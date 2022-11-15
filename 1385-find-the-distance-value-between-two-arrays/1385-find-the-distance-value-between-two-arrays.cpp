e0f2dd851d70ed67367fbbb69c991432682dec71
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2022-11-15 18:36:19 +0530

    Time: 15 ms (81.51%), Space: 13.2 MB (33.37%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        int n1 = arr1.size();
-        int n2 = arr2.size();
-        int count = 0;
-        int i=0;
         
-        while(i<n1){
-            bool flag = false;
-            int j=0;
-            while(j<n2 && !flag){
-                if(abs(arr1[i] - arr2[j]) <= d)
-                    flag = true;
-                j++;
-            }
-            if(!flag)
-                count++;
-            i++;
+        //Brute Force
+        
+//         int n1 = arr1.size();
+//         int n2 = arr2.size();
+//         int count = 0;
+//         int i=0;
+        
+//         while(i<n1){
+//             bool flag = false;
+//             int j=0;
+//             while(j<n2 && !flag){
+//                 if(abs(arr1[i] - arr2[j]) <= d)
+//                     flag = true;
+//                 j++;
+//             }
+//             if(!flag)
+//                 count++;
+//             i++;
+//         }
+        
+//         return count;
+        
+        //Binary Search
+        
+        sort(arr2.begin(),arr2.end());
+        int ans = 0;
+        for(auto it:arr1){
+            if(upper_bound(arr2.begin(),arr2.end(),it+d) == lower_bound(arr2.begin(),arr2.end(),it-d))
+                ans++;
         }
-        
-        return count;
+        return ans;
     }
 };
