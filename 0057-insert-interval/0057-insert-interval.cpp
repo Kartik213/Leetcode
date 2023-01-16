7b490aec8760b964fc1f4811905183361ab8dd57
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-01-16 20:33:04 +0530

    Time: 20 ms (69.28%), Space: 17.1 MB (45.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-         vector<vector<int>>ans;
-        for(auto &&i:intervals){
-    //  the new interval is after the range of other interval
-            if(i[1]<newInterval[0])ans.push_back(i);
-    // the new interval's range is before the other
-            else if(newInterval[1]<i[0]){
-                ans.push_back(newInterval);
-                newInterval=i;//updating the new interval
+        vector<vector<int>>ans;
+        int n = intervals.size();
+        for(int i=0;i<n;i++){
+            if(intervals[i][1] < newInterval[0]){
+                ans.push_back(intervals[i]);
             }
-    // the new interval is in the range of the other interval
+            else if(newInterval[1]<intervals[i][0]){
+                ans.push_back(newInterval);
+                newInterval = intervals[i];
+            }
             else{
-                newInterval[0]=min(newInterval[0],i[0]);
-                newInterval[1]=max(newInterval[1],i[1]);
-            }    
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+            }
         }
-    //At the end after the loop just add the updated newIntervals
         ans.push_back(newInterval);
         return ans;
     }
