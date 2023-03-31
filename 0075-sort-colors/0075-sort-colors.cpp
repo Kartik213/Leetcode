955d30e0b74f04647864d7ff21d23fbf8dc99d00
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-03-31 11:55:16 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (32.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero = 0, one = 0, two = 0;
+        // int zero = 0, one = 0, two = 0;
+        // for(auto it: nums){
+        //     if(it == 0)
+        //         zero++;
+        //     else if(it == 1)
+        //         one++;
+        //     else
+        //         two++;
+        // }
+        // int i=0;
+        // for(i=0;i<zero;i++){
+        //     nums[i] = 0;
+        // }
+        // for(i;i<one+zero;i++){
+        //     nums[i] = 1;
+        // }
+        // for(i;i<two+one+zero;i++){
+        //     nums[i] = 2;
+        // }
+        vector<int> freq(3,0);
         for(auto it: nums){
-            if(it == 0)
-                zero++;
-            else if(it == 1)
-                one++;
-            else
-                two++;
+            freq[it]++;
         }
-        int i=0;
-        for(i=0;i<zero;i++){
-            nums[i] = 0;
-        }
-        for(i;i<one+zero;i++){
-            nums[i] = 1;
-        }
-        for(i;i<two+one+zero;i++){
-            nums[i] = 2;
+        int i=0, j=0;
+        while(i<3){
+            if(freq[i]>0){
+                nums[j] = i;
+                j++;
+                freq[i]--;
+            }else{
+                i++;
+            }
+            
         }
     }
 };
