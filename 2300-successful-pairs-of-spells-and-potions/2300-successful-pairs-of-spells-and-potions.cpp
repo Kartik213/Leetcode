02677dda9c2b32f2ce3293e310b92a0f9ae8d0a5
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-04-02 19:39:22 +0530

    Time: 261 ms (49.89%), Space: 97.1 MB (86.98%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -6,37 +6,37 @@ public:
         vector<int> pairs(n, 0);
         sort(potions.begin(), potions.end());
         for (int i = 0; i < n; i++) {
-            int spell = spells[i];
-            int left = 0;
-            int right = m - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                long long product = (long long)spells[i] * (long long)potions[mid];
-                if (product >= success) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            // int spell = spells[i];
+            // int left = 0;
+            // int right = m - 1;
+            // while (left <= right) {
+            //     int mid = left + (right - left) / 2;
+            //     long long product = (long long)spells[i] * (long long)potions[mid];
+            //     if (product >= success) {
+            //         right = mid - 1;
+            //     } else {
+            //         left = mid + 1;
+            //     }
+            // }
+            // pairs[i] = m - left;
+            long long product = (long long)spells[i] * (long long)potions[m-1];
+            if(product < success){
+                pairs[i] = 0;
             }
-            pairs[i] = m - left;
-        //     long long product = (long long)spells[i] * (long long)potions[m-1];
-        //     if(product < success){
-        //         pairs[i] = 0;
-        //     }
-        //     else{
-        //         int left = 0;
-        //         int right = m - 1;
-        //         while (left <= right) {
-        //             int mid = left + (right - left) / 2;
-        //             product = (long long)spells[i] * (long long)potions[mid];
-        //             if (product >= success) {
-        //                 right = mid - 1;
-        //             } else {
-        //                 left = mid + 1;
-        //             }
-        //         }
-        //         pairs[i] = m - left;
-        //     }
+            else{
+                int left = 0;
+                int right = m - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    product = (long long)spells[i] * (long long)potions[mid];
+                    if (product >= success) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                pairs[i] = m - left;
+            }
         }
         return pairs;
     }
