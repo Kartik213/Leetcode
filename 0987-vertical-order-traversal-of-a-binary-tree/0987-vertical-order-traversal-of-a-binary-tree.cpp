398de46ca01cfbe31a8ac171c29bf70e9da7c5cd
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-08-03 00:21:16 +0530

    Time: 4 ms (70.08%), Space: 12.5 MB (82.74%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,30 +11,40 @@
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode* , pair<int,int>>>q;
-        q.push({root,{0,0}});
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-            TreeNode* node = temp.first;
-            int x = temp.second.first;
-            int y = temp.second.second;
-            nodes[x][y].insert(node->val);
-            
-            if(node->left != NULL)
-                q.push({node->left , {x-1,y+1}});
-            if(node->right != NULL)
-                q.push({node->right , {x+1,y+1}});
+    map<int, map<int, vector<int>>> nodes;
+    void solve(TreeNode* root){
+        if(!root){
+            return ;
         }
-        vector<vector<int>>ans;
-        for(auto it:nodes){
-            vector<int>col;
-            for(auto x:it.second){
-                col.insert(col.end(),x.second.begin(),x.second.end());
+        queue<pair<TreeNode*, pair<int, int>>>q;
+        q.push(make_pair(root, make_pair(0, 0)));
+        while(!q.empty()){
+            pair<TreeNode*, pair<int, int>> temp = q.front();
+            q.pop();
+            TreeNode* tempNode = temp.first;
+            int horizontal = temp.second.first;
+            int lvl = temp.second.second;
+            nodes[horizontal][lvl].push_back(tempNode->val);
+            if(tempNode->left){
+                q.push(make_pair(tempNode->left, make_pair(horizontal-1, lvl+1)));
             }
-            ans.push_back(col);
+            if(tempNode->right){
+                q.push(make_pair(tempNode->right, make_pair(horizontal+1, lvl+1)));
+            }
+        }
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        solve(root);
+        for(auto i:nodes){
+            vector<int>temp;
+            for(auto j:i.second){
+                sort(j.second.begin(), j.second.end());
+                for(auto k:j.second){
+                    temp.push_back(k);
+                }
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
