20875cf3941dde35fec564d69b7d85c3ebf191ce
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-08-02 10:41:20 +0530

    Added solution - LeetHub

--- Level order traversal - GFG/level-order-traversal.cpp
+++ Level order traversal - GFG/level-order-traversal.cpp
@@ -42,24 +42,22 @@ class Solution
 {
     public:
     //Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* node)
-    {
-        vector<int>res;
+    vector<int> levelOrder(Node* node){
+        vector<int>ans;
         queue<Node*>q;
         q.push(node);
         while(!q.empty()){
-            int n = q.size();
-            for(int i=0;i<n;i++){
-                Node* temp = q.front();
-                q.pop();
-                if(temp->left != NULL)
-                    q.push(temp->left);
-                if(temp->right != NULL)
-                    q.push(temp->right);
-                res.push_back(temp->data);
+            Node* temp = q.front();
+            q.pop();
+            ans.push_back(temp->data);
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
             }
         }
-        return res;
+        return ans;
     }
 };
 
