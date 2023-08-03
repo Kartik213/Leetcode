f18c9e6a5a39fd32ba935dd56599a8a98c5c1ba0
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-08-03 16:21:43 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -103,29 +103,29 @@ class Solution
     //Function to return a list of nodes visible from the top view 
     //from left to right in Binary Tree.
     vector<int> topView(Node *root){
-        vector<int>res;
-        queue<pair<int,Node*>>q;
-        map<int,int>mp;
-        q.push({0,root});
+        vector<int>ans;
+        map<int, int> nodes;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
         while(!q.empty()){
-            auto it = q.front();
+            auto temp = q.front();
             q.pop();
-            Node* temp = it.second;
-            int x = it.first;
-            if(mp.find(x) == mp.end()){
-                mp[x] = temp->data;
+            Node* tempNode = temp.first;
+            int horizontalDis = temp.second;
+            if(nodes.find(horizontalDis) == nodes.end()){
+                nodes[horizontalDis] = tempNode->data;
             }
-            if(temp->left != NULL){
-                q.push({x-1,temp->left});
+            if(tempNode->left){
+                q.push({tempNode->left, horizontalDis-1});
             }
-            if(temp->right != NULL){
-                q.push({x+1,temp->right});
+            if(tempNode->right){
+                q.push({tempNode->right, horizontalDis+1});
             }
         }
-        for(auto it:mp){
-            res.push_back(it.second);
+        for(auto i:nodes){
+            ans.push_back(i.second);
         }
-        return res;
+        return ans;
     }
 
 };
