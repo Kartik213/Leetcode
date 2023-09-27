2164493225465bcfd523bd0f2d9cc018564995ac
Kartik213 <83511783+Kartik213@users.noreply.github.com>
2023-09-27 12:43:45 +0530

    Time: 8 ms (54.19%) | Memory: 7.9 MB (30.80%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -6,17 +6,37 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_set<ListNode*>st;
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             if(st.find(temp) != st.end()){
+//                 return temp;
+//             }
+//             st.insert(temp);
+//             temp = temp->next;
+//         }
+//         return NULL;
+//     }
+// };
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*>st;
+        ListNode* slow = head;
+        ListNode* fast = head;
         ListNode* temp = head;
-        while(temp != NULL){
-            if(st.find(temp) != st.end()){
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
+                while(temp != slow){
+                    temp = temp->next;
+                    slow = slow->next;
+                }
                 return temp;
             }
-            st.insert(temp);
-            temp = temp->next;
         }
         return NULL;
     }
