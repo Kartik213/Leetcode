ab07173b3a5004f85e5134cf72240ed8c5082b75
Kartik Gupta <83511783+Kartik213@users.noreply.github.com>
2024-01-02 09:22:32 +0530

    Time: 23 ms (9.84%) | Memory: 30.9 MB (10.21%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        map<int, int>mp;
+        int maxFreq = 0;
+        for (int it:nums ){
+            mp[it]++;
+            maxFreq = max(maxFreq, mp[it]);
+        }
+        vector<vector<int>>ans(maxFreq);
+        vector<set<int>>check(maxFreq);
+
+        for(int it: nums){
+            for(int i=0;i<maxFreq; i++){
+                if(check[i].find(it) == check[i].end()){
+                    ans[i].push_back(it);
+                    check[i].insert(it);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
